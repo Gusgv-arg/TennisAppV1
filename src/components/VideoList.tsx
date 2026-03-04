cb04293bb015a390d752e33aed44d2eb266c375f
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2026-03-04 17:57:54 -0300

    mensaje compartir video2

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -252,8 +252,8 @@ export default function VideoList({ playerId }: VideoListProps) {
             const textToShare = `🎾 ¡Te compartieron tu video desde Tenis-Lab!\n${video.title}`;
 
             await Share.share({
-                message: Platform.OS === 'web' ? '' : (Platform.OS === 'android' ? `${textToShare}\n${url}` : textToShare),
-                url: Platform.OS === 'android' ? undefined : (Platform.OS === 'web' ? `${textToShare}\n${url}` : url),
+                message: Platform.OS === 'android' ? `${textToShare}\n${url}` : textToShare,
+                url: Platform.OS === 'android' ? undefined : url,
                 title: video.title
             });
         } catch (error: any) {
